8f451a5df49fba33b3ccb55fed34b721cc4e0474
Agustín Comolli <agustincomolli@gmail.com>
2023-11-29 13:08:19 -0300

    fix(back): change style in main tag
    feat(back): add zebra style to service table
    
    feat(back): add service.html

--- app/routes.py
+++ app/routes.py
@@ -31,4 +31,43 @@ def index():
         "services": Service.query.all(),
     }
 
-    return render_template('admin.html', **context)
+    return render_template('admin-services.html', **context)
+
+
+@app.route("/add-service", methods=["GET", "POST"])
+def add_service():
+    """
+    Ruta para agregar un nuevo servicio.
+
+    Método GET:
+    Muestra el formulario para agregar un nuevo servicio.
+
+    Método POST:
+    Procesa el formulario enviado, crea un nuevo servicio y lo agrega a la base de datos.
+
+    Returns:
+    - GET: Renderiza el formulario para agregar un servicio.
+    - POST: Redirecciona a la página principal después de agregar el servicio.
+    """
+    if request.method == "POST":
+        # Obtiene los datos del formulario enviado por el método POST
+        name = request.form["name"]
+        description = request.form["description"]
+
+        # Crea un nuevo objeto Service con los datos proporcionados
+        new_service = Service(name=name, description=description)
+
+        # Agrega el nuevo servicio a la sesión de la base de datos
+        db.session.add(new_service)
+
+        # Guarda los cambios en la base de datos
+        db.session.commit()
+
+        # Redirecciona a la página principal después de agregar el servicio
+        return redirect(url_for("index"))
+    
+    context = {
+        "title": "Agrega un nuevo servicio",
+    }
+    # Renderiza el formulario para agregar un servicio (Método GET)
+    return render_template("service.html", **context)
